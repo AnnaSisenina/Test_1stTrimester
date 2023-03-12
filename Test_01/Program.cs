ecba504522dfd1bf823ccb013cacc6d2fc50b3a5
Anna Sisenina <yuta4ka71@gmail.com>
2023-03-12 21:23:25 +0300

    Magical number '3' replaced by int requiredLength

--- Test_01/Program.cs
+++ Test_01/Program.cs
@@ -12,19 +12,21 @@ for (int i = 0; i < strings.Length; i++)
     strings[i] = Console.ReadLine()!;
 
 int count=0;
+int requiredLength=3;
 for (int i = 0; i < strings.Length; i++)
 {
-    if (strings[i].Length<=3) count++;
+    if (strings[i].Length<=requiredLength) count++;
 }
 string [] shortsStrings = new string [count];
 for (int i = 0, j=0; i < strings.Length; i++)
 {
-    if (strings[i].Length<=3) 
+    if (strings[i].Length<=requiredLength) 
     {
         shortsStrings[j]=strings[i];
         j++;
     }
 }
-Console.WriteLine("Строки длина, которых меньше или равна 3");
+Console.WriteLine();
+Console.WriteLine($"Строки длина, которых меньше или равна {requiredLength}:");
 for (int i = 0; i < shortsStrings.Length; i++) Console.WriteLine($"{shortsStrings[i]}\t");
 
