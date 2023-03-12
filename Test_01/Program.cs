2f5a0cb6de552690122b0d58e1b4d14629e3c992
Anna Sisenina <yuta4ka71@gmail.com>
2023-03-12 22:34:48 +0300

    method PrintArray added

--- Test_01/Program.cs
+++ Test_01/Program.cs
@@ -2,6 +2,13 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.WriteLine($"{array[i]}\t");
+}
+
+
 int quantity;
 Console.WriteLine("Введите количество строк, которое Вы хотите ввести:");
 quantity = int.Parse(Console.ReadLine()!);
@@ -11,22 +18,23 @@ Console.WriteLine("Введите строки через 'Enter'");
 for (int i = 0; i < strings.Length; i++)
     strings[i] = Console.ReadLine()!;
 
-int count=0;
-int requiredLength=3;
+int count = 0;
+int requiredLength = 3;
 for (int i = 0; i < strings.Length; i++)
 {
-    if (strings[i].Length<=requiredLength) count++;
+    if (strings[i].Length <= requiredLength) count++;
 }
-string [] shortsStrings = new string [count];
-for (int i = 0, j=0; i < strings.Length; i++)
+
+string[] shortsStrings = new string[count];
+for (int i = 0, j = 0; i < strings.Length; i++)
 {
-    if (strings[i].Length<=requiredLength) 
+    if (strings[i].Length <= requiredLength)
     {
-        shortsStrings[j]=strings[i];
+        shortsStrings[j] = strings[i];
         j++;
     }
 }
 Console.WriteLine();
-Console.WriteLine($"Строки длина, которых меньше или равна {requiredLength}:");
-for (int i = 0; i < shortsStrings.Length; i++) Console.WriteLine($"{shortsStrings[i]}\t");
+Console.WriteLine($"Строки, длина которых меньше или равна {requiredLength}:");
+PrintArray(shortsStrings);
 
